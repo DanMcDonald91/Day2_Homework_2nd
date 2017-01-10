9601a77ee5eebdf7f88fad3f5ce26e26eafcf2f2
Daniel McDonald <danielkg91@gmail.comssh-keygen -t rsa -C danielkg91@gmail.com>
2017-01-10 19:00:08 +0000

    This is the length of my name

---
+++ length_of_name.rb
@@ -0,0 +1,5 @@
+# this is the length of name function
+def length_of_name(Daniel, McDonald) 
+  return length_of_name
+end
+puts length_of_name = 14
---
+++ name_length.rb
@@ -0,0 +1,6 @@
+# this is the length of name function
+def length_of_name(first_name, second_name)
+return first_name.length + second_name.length
+end
+puts length_of_name("Daniel","mcdonald")
+
