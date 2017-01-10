76f148fbc0071610c65ea92f19c2aaab7af69579
Daniel McDonald <danielkg91@gmail.comssh-keygen -t rsa -C danielkg91@gmail.com>
2017-01-10 22:15:49 +0000

    The bank and pin function

---
+++ bank_balance.rb
@@ -0,0 +1,16 @@
+# This is the bank balance function
+def bank_balance(pin_code)
+if pin_code == 1234
+  puts " your balance is 999.99"
+else
+  puts "incorrect pin"
+end
+end
+puts " Enter pin now"
+pin_code = gets.chomp.to_i
+bank_balance(pin_code)
+
+
+
+
+
