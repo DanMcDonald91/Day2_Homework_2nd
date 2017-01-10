f02aa781650c76397b36024a7b9413a736926019
DanMcDonald91 <danielmcdonald91kg@gmail.com>
2017-01-10 19:20:52 +0000

    Update name_length.rb

--- name_length.rb
+++ name_length.rb
@@ -1,4 +1,4 @@
-# this is the length of name function
+# this is the length of name function- correct version- dont know how to delete other one
 def length_of_name(first_name, second_name)
 return first_name.length + second_name.length
 end
