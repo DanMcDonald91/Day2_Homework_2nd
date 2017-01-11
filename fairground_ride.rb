1cd1427e0ef3521e20ab6cf3563334bc1ff0b6ac
Daniel McDonald <danielkg91@gmail.comssh-keygen -t rsa -C danielkg91@gmail.com>
2017-01-11 13:34:30 +0000

    Fairground ride function

---
+++ fairground_ride.rb
@@ -0,0 +1,12 @@
+# Fairground Ride Height And Weight
+def fairground (weight, height)
+  if (weight <=40 && height <=1.4)
+    return " You are free to ride"
+  else
+    return "Sorry your are not allowed to ride"
+  end
+end
+ puts fairground( 40, 1.4)
+ 
+
+
